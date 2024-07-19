8d5ab85ea383fce9b5bb16c7cf337709390ee055
Zooz <zahraa95emad@gmail.com>
2024-07-19 11:50:03 +0200

    Modal

--- src/components/list1.js
+++ src/components/list1.js
@@ -9,22 +9,59 @@ import * as React from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import { styled } from "@mui/system";
+
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const StyledImg = styled("img")({
+  maxHeight: "90%",
+  maxWidth: "90%",
+});
 
 export default function List1() {
+  const [open, setOpen] = React.useState(false);
+  const [selectedImg, setSelectedImg] = React.useState(null);
+
+  const handleOpen = (img) => {
+    setSelectedImg(img);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedImg(null);
+  };
+
   return (
     <Box sx={{ marginLeft: "10%" }}>
       <ImageList variant="masonry" cols={2} gap={6}>
         {itemData.map((item) => (
           <ImageListItem key={item.img}>
             <img
-              style={{ width: "80%" }}
+              style={{ width: "80%", cursor: "pointer" }}
               srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
               src={`${item.img}?w=248&fit=crop&auto=format`}
               alt={item.title}
+              onClick={() => handleOpen(item.img)}
             />
           </ImageListItem>
         ))}
       </ImageList>
+      {selectedImg && (
+        <StyledModal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="image-modal-title"
+          aria-describedby="image-modal-description"
+        >
+          <StyledImg src={selectedImg} alt="Selected" />
+        </StyledModal>
+      )}
     </Box>
   );
 }
