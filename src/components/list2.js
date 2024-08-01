a77e63bf13f0e832acabad6710c0a513e301cde0
Zooz <zahraa95emad@gmail.com>
2024-08-01 15:21:31 +0200

    no gallery

--- src/components/list2.js
+++ src/components/list2.js
@@ -15,17 +15,12 @@ import Img14 from "../images/b/b14.jpeg";
 import Img15 from "../images/b/b15.jpeg";
 import Img16 from "../images/b/b16.jpeg";
 import Img17 from "../images/b/b17.jpeg";
-
 import * as React from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
-import IconButton from "@mui/material/IconButton";
-import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { styled } from "@mui/system";
-import { useSwipeable } from "react-swipeable";
 
 const StyledModal = styled(Modal)({
   display: "flex",
@@ -38,98 +33,43 @@ const StyledImg = styled("img")({
   maxWidth: "90%",
 });
 
-const ArrowButton = styled(IconButton)({
-  position: "absolute",
-  top: "50%",
-  transform: "translateY(-50%)",
-  color: "white",
-  backgroundColor: "rgba(0, 0, 0, 0.5)",
-  "&:hover": {
-    backgroundColor: "rgba(0, 0, 0, 0.7)",
-  },
-  zIndex: 1000, // Ensure buttons are on top
-});
-
-const ArrowButtonLeft = styled(ArrowButton)({
-  left: "-60px", // Position outside the left of the image
-});
-
-const ArrowButtonRight = styled(ArrowButton)({
-  right: "-60px", // Position outside the right of the image
-});
-
-export default function List1() {
+export default function List2() {
   const [open, setOpen] = React.useState(false);
-  const [selectedIndex, setSelectedIndex] = React.useState(null);
+  const [selectedImg, setSelectedImg] = React.useState(null);
 
-  const handleOpen = (index) => {
-    setSelectedIndex(index);
+  const handleOpen = (img) => {
+    setSelectedImg(img);
     setOpen(true);
   };
 
   const handleClose = () => {
     setOpen(false);
-    setSelectedIndex(null);
+    setSelectedImg(null);
   };
-
-  const handleNext = () => {
-    setSelectedIndex((prevIndex) => (prevIndex + 1) % itemData.length);
-  };
-
-  const handlePrev = () => {
-    setSelectedIndex(
-      (prevIndex) => (prevIndex - 1 + itemData.length) % itemData.length
-    );
-  };
-
-  const handlers = useSwipeable({
-    onSwipedLeft: handleNext,
-    onSwipedRight: handlePrev,
-    preventDefaultTouchmoveEvent: true,
-    trackMouse: true,
-  });
 
   return (
     <Box sx={{ marginLeft: "10%" }}>
       <ImageList variant="masonry" cols={2} gap={6}>
-        {itemData.map((item, index) => (
+        {itemData.map((item) => (
           <ImageListItem key={item.img}>
             <img
               style={{ width: "80%", cursor: "pointer" }}
               srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
               src={`${item.img}?w=248&fit=crop&auto=format`}
               alt={item.title}
-              onClick={() => handleOpen(index)}
+              onClick={() => handleOpen(item.img)}
             />
           </ImageListItem>
         ))}
       </ImageList>
-      {selectedIndex !== null && (
+      {selectedImg && (
         <StyledModal
           open={open}
           onClose={handleClose}
           aria-labelledby="image-modal-title"
           aria-describedby="image-modal-description"
         >
-          <div
-            {...handlers}
-            style={{
-              position: "relative",
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <ArrowButtonLeft onClick={handlePrev} aria-label="previous">
-              <ArrowBackIosNewIcon />
-            </ArrowButtonLeft>
-            <StyledImg
-              src={itemData[selectedIndex].img}
-              alt={itemData[selectedIndex].title}
-            />
-            <ArrowButtonRight onClick={handleNext} aria-label="next">
-              <ArrowForwardIosIcon />
-            </ArrowButtonRight>
-          </div>
+          <StyledImg src={selectedImg} alt="Selected" />
         </StyledModal>
       )}
     </Box>
